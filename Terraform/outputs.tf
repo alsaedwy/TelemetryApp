7e057d1178e4c42ea717b348bd2571186969fa49
Alaa Saeed <alsaedwy@gmail.com>
2021-10-03 19:14:40 +0100

    Adjusting Outputs

--- Terraform/outputs.tf
+++ Terraform/outputs.tf
@@ -1,12 +1,3 @@
-<<<<<<< HEAD
-output "ECR-Repo-URL" {
-=======
-output "ECR-Repo-URL" {
->>>>>>> 33ddeb7fce0f64e7f38696f1c9c187a40cca82d6
-  value = aws_ecr_repository.TelemetryAppECRRepo.repository_url
-}
-
-
 output "CodeCommit-RepositoryURL" {
   value = aws_codecommit_repository.TelemetryApp-CC-Repo.clone_url_http
 }
